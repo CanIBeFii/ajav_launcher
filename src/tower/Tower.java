a79035f3a46911a29c86dcac285248aa55beac3e
CanIBeFii <filipe250502@gmail.com>
2025-03-06 14:25:57 +0000

    feat: add basic implementation of tower

---
+++ src/tower/Tower.java
@@ -0,0 +1,22 @@
+package tower;
+
+import java.util.ArrayList;
+import flyable.Flyable;
+
+public class Tower {
+	private ArrayList<Flyable> observers;
+
+	public Tower() {
+		this.observers = new ArrayList<Flyable>();
+	}
+
+	public void register(Flyable p_flyable) {
+		this.observers.add(p_flyable);
+	}
+
+	public void unregister(Flyable p_flyable) {
+		this.observers.remove(p_flyable);
+	}
+
+	protected void conditionChanged() {}
+}
