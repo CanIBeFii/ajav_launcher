a012ed9fe48f7e445acaa5ad391f4e216dc4dcf1
CanIBeFii <filipe250502@gmail.com>
2025-03-07 13:14:20 +0000

    feat: implement WeatherTower and randomize the weather conditions

--- src/tower/WeatherTower.java
+++ src/tower/WeatherTower.java
@@ -1,5 +1,32 @@
 package tower;
 
-public class WeatherTower {
-	
+import java.util.Random;
+
+import flyable.Coordinates;
+
+public class WeatherTower extends Tower{
+	private int weatherVariable;
+	private Random numberGenerator;
+	private final static String[] weather = {"RAIN", "SUN", "SNOW", "FOG"};
+
+	public WeatherTower() {
+		super();
+		this.weatherVariable = 0;
+		this.numberGenerator =  new Random();
+	}
+
+	public String getWeather(Coordinates p_coordinates) {
+		String newWeather = weather[p_coordinates.getLongitude() 
+					+ p_coordinates.getLatitude()
+					+ p_coordinates.getHeight()
+					+ weatherVariable];
+		return newWeather;
+	}
+
+	public void changeWeather() {
+		this.weatherVariable = this.numberGenerator.nextInt(4);
+		conditionChanged();
+	}
 }
+
+
