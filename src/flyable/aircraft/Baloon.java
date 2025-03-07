2a94d37df226530795fb2d7706edd80521d7ff3e
CanIBeFii <filipe250502@gmail.com>
2025-03-07 15:20:35 +0000

    fix: typo for Baloon

--- src/flyable/aircraft/Baloon.java
+++ src/flyable/aircraft/Baloon.java
@@ -2,9 +2,9 @@ package flyable.aircraft;
 
 import flyable.Coordinates;
 
-public class Ballon extends Aircraft {
+public class Baloon extends Aircraft {
 
-	public Ballon(long p_id, String p_name, Coordinates p_coordinates) {
+	public Baloon(long p_id, String p_name, Coordinates p_coordinates) {
 		super(p_id, p_name, p_coordinates);
 	}
 
---
+++ src/flyable/aircraft/factory/BaloonFactory.java
@@ -0,0 +1,19 @@
+package flyable.aircraft.factory;
+
+import flyable.Coordinates;
+import flyable.aircraft.Aircraft;
+import flyable.aircraft.Baloon;
+
+public class BaloonFactory implements Factory{
+	private final static BaloonFactory instance = new BaloonFactory();
+
+	private BaloonFactory() {}
+
+	public static BaloonFactory getInstance() {
+		return instance;
+	}
+
+	public Aircraft newAircraft(long p_id, String p_name, Coordinates p_coordinates) {
+		return new Baloon(p_id, p_name, p_coordinates);
+	}
+}
